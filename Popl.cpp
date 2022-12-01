1f1df01d42e53ee04255b30475eb03a66c7d86b3
radha-krishna-b <85828043+radha-krishna-b@users.noreply.github.com>
2022-12-01 10:39:33 +0530

    Update Popl.cpp

--- Popl.cpp
+++ Popl.cpp
@@ -3,40 +3,13 @@
 #include<map>
 #include<vector>
 #include<cmath>
-#include<algorithm>\
+#include<algorithm>
 #include<cctype>
-
 
 using namespace std;
 
-
 string table_name = "STORE";
 
-/*
-Concrete Syntax
----------------
-
-    (Expression) (Valid if in intital environment)
-    (Expression) ( Expression relationop Number ) ((Valid Relation operators are >,<,>=,<=,!=,=) Expression should be a valid)
-    (Expression) ( Expression relationop String ) ((Valid Relational Operators are = , !=) Expression should be a valid)
-    (Expression) ( Expression relationop Number/String ) logicalop (Expression relationop Number/String) ((Valid logical operators are AND, OR) (both the conditions should be a valid expression))
-
-
-
-    Valid Expressions:
-    ------------------
-
-    1.  ItemName
-    2.  ItemCost
-    3.  ItemColor
-    4.  ItemQuantity
-    5.  ItemName (Item_Name = "Lays")
-    6.  ItemQuantity (Item_Name = "Lays") AND/OR (Item_Color = "green")
-    7.  ItemName (NOT Item_Name = "Lays")
-    8.  ItemName (Item_Name = "Lays") AND/OR (Item_Quantity > 10)
-
-
-*/
 
 vector<string> get_words (string expression){
 
@@ -69,45 +42,27 @@ vector<string> get_words (string expression){
 }
 
 
+bool is_relation_operator(string expression){
 
-int count_words(string expression){
-    int count = 0;
-    if(expression.size() == 0){
-        return count;
+    if((expression == "=") || (expression == "<") || (expression == "!=") || (expression == ">") || (expression == ">=")
+    || (expression == "<=")){
+        return true;
     }
     else{
-        bool omit_white_space = false;
-        bool is_alpha = false;
-        for(int i=0;i<expression.size();i++){
-            if((is_alpha == true) && (expression[i] == ' ') && (omit_white_space == false)){
-                count += 1;
-                omit_white_space = true;
-            }
-            else if((expression[i] != ' ') && (omit_white_space == true)){
-                omit_white_space = false;
-            }
-            else if(expression[i] != ' '){
-                is_alpha = true;
-            }
-        }
+        return false;
     }
-    return count+1;
-
 }
 
-bool is_number (string expression){
+bool is_logical_operator(string expression){
 
-    for(int i =0;i<expression.size();i++){
-        int temp = expression[i];
-        if((temp >= '0') && (temp <='9')){
-            continue;
-        }
-        else{
-            return false;
-        }
+    if((expression == "AND") || (expression == "OR") || (expression == "NOT")){
+        return true;
     }
-    return true;
+    else{
+        return false;
+    }
 }
+
 
 bool is_alpha_string(string expression){
 
@@ -141,59 +96,63 @@ bool is_alpha_string(string expression){
 
 }
 
+bool is_number (string expression){
 
-bool lookup_initial_environment(string expression , vector<string> initial_environment){
+    for(int i =0;i<expression.size();i++){
+        int temp = expression[i];
+        if((temp >= '0') && (temp <='9')){
+            continue;
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+}
 
-    for(int i =0;i<initial_environment.size();i++){
 
-        if(initial_environment[i] == expression){
-            return true;
+bool parse_column(string expression, vector<string> initial_environment,string& parsed_string){
+
+    vector<string> cols = get_words(expression);
+    for(int i = 0;i<cols.size();i++){
+        bool found = false;
+        for(int j = 0; j<initial_environment.size();j++){
+            if(cols[i] == initial_environment[j]){
+                found = true;
+                string curr = "";
+                curr += ("( id " + (cols[i]) + " )");
+                parsed_string += curr;
+                break;
+            }
+        }
+        if(found == false){
+            return false;
         }
     }
 
-    return false;
+    return true;
+
 }
 
-int lookup_extended_environment(string expression, map<string,int> extended_environment){
 
-    return 0;
-}
-
-bool is_relation_operator(string expression){
-
-    if((expression == "=") || (expression == "<") || (expression == "!=") || (expression == ">") || (expression == ">=")
-    || (expression == "<=")){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-bool is_logical_operator(string expression){
-
-    if((expression == "AND") || (expression == "OR") || (expression == "NOT")){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
 
 bool parse_expression(string expression, vector<string> initial_environment,
-map<string,int> extended_environment, string& parsed_string){
+map<string,int> extended_environment, string& parsed_string, vector<int>& check_statements){
 
-    int number_words = count_words(expression);
+    vector<string> words = get_words(expression);
+    int number_words = words.size();
     //cout<<"Number of Words : "<<number_words<<endl;
     cout<<endl;
     cout<<"Parsing . . . "<<expression<<" "<<endl<<endl;
     if(number_words == 1){
         if(is_alpha_string(expression) == true){
-            if(lookup_initial_environment(expression,initial_environment) == true){
+            string temp_10 = "";
+            if(parse_column(expression,initial_environment,temp_10) == true){
                 cout<<"Parsed successfully : "<<expression<<endl<<endl;
                 string curr = "";
                 curr += ("( id " + (expression) + " )");
                 parsed_string += curr;
+                check_statements.push_back(1);
                 return true;
             }
             else if(( is_logical_operator(expression) == true ) && (extended_environment[expression] == -1)){
@@ -201,6 +160,7 @@ map<string,int> extended_environment, string& parsed_string){
                 curr += ("( logicalop " + (expression) + " )");
                 parsed_string += curr;
                 cout<<"Parsed successfully : "<<expression<<endl<<endl;
+                check_statements.push_back(2);
                 return true;
             }
             else if(extended_environment[expression] == -1){
@@ -208,6 +168,7 @@ map<string,int> extended_environment, string& parsed_string){
                 curr += ("( string " + (expression) + " )");
                 parsed_string += curr;
                 cout<<"Parsed successfully : "<<expression<<endl<<endl;
+                check_statements.push_back(3);
                 return true;
             }
             else{
@@ -219,6 +180,7 @@ map<string,int> extended_environment, string& parsed_string){
             if(is_relation_operator (expression) == true){
                 if(extended_environment[expression] == 0){
                      cout<<"Invalid Expression : Not a valid Column Name"<<endl;
+
                      return false;
                 }
                 else{
@@ -226,6 +188,7 @@ map<string,int> extended_environment, string& parsed_string){
                     curr += ("( relationalop " + (expression) + " )");
                     parsed_string += curr;
                     cout<<"Parsed successfully : "<<expression<<endl<<endl;
+                    check_statements.push_back(0);
                     return true;
                 }
             }
@@ -239,6 +202,7 @@ map<string,int> extended_environment, string& parsed_string){
                     curr += ("( num " + (expression) + " )");
                     parsed_string += curr;
                     cout<<"Parsed successfully : "<<expression<<endl<<endl;
+                    check_statements.push_back(4);
                     return true;
                 }
             }
@@ -251,20 +215,15 @@ map<string,int> extended_environment, string& parsed_string){
     }
     else{
 
-        vector<string> words = get_words(expression);
-        bool flag_1 = parse_expression(words[0],initial_environment,extended_environment,parsed_string);
         vector<string> temp_words;
-
-
-        if(flag_1 == true){
-            vector<string> parenthesis_stack;
+        vector<string> parenthesis_stack;
             bool is_started = false;
-            for(int i = 1;i<words.size();i++){
+            for(int i = 0;i<words.size();i++){
 
                 if(is_logical_operator(words[i]) == true){
                     if(words[i-1] == ")" && (((i+1) < words.size()) && words[i+1] == "(")){
                         extended_environment[words[i]] = -1;
-                        bool flag = parse_expression(words[i],initial_environment,extended_environment,parsed_string);
+                        bool flag = parse_expression(words[i],initial_environment,extended_environment,parsed_string,check_statements);
                         if(flag == false){
                             return false;
                         }
@@ -297,7 +256,7 @@ map<string,int> extended_environment, string& parsed_string){
                             }
 
                             extended_environment[temp_words[j]] = -1;
-                            bool flag = parse_expression(temp_words[j],initial_environment,extended_environment,parsed_string);
+                            bool flag = parse_expression(temp_words[j],initial_environment,extended_environment,parsed_string,check_statements);
                             if(flag == false){
                                 return false;
                             }
@@ -318,93 +277,200 @@ map<string,int> extended_environment, string& parsed_string){
                 return false;
             }
             return true;
-
-        }
     }
 }
 
-string interpret_expression(string expression, vector<string> initial_environment,
-map<string,int> extended_environment, vector<string> commands){
+
+string interpret_expression(string expression, vector<string> commands){
 
     string sql_statement = "";
-
-    int number_words = count_words(expression);
-
-    if(number_words == 1){
-        sql_statement += (commands[0] + " " + expression + " " + commands[1] + " " + table_name);
+    if(expression.size() == 0){
         return sql_statement;
     }
-    else if(number_words = 6){
-
-        vector<string> words = get_words(expression);
-        sql_statement = interpret_expression(words[0],initial_environment,extended_environment,commands);
-        sql_statement += (" " + commands[2] + " ");
-        for(int i  = 1;i<words.size()-1;i++){
-            sql_statement += (words[i] + " ");
-        }
-        sql_statement += words[words.size()-1];
-        return sql_statement;
+    else{
+       sql_statement = commands[0] + " ";
+       vector<string> words = get_words(expression);
+       for(int i = 0;i<words.size()-1;i++){
+            sql_statement += words[i] + ",";
+       }
+       sql_statement += words[words.size()-1] + " ";
+       sql_statement += commands[1] + " " +table_name;
     }
-
-
     return sql_statement;
 }
 
+string interpret_condition(string expression, vector<string> commands){
+
+
+    string sql_statement = "";
+
+    if(expression.size() == 0){
+        return sql_statement;
+    }
+    else{
+        sql_statement = commands[2] + " " + expression;
+    }
+    return sql_statement;
+}
+
+bool check_valid_condition(vector<int> values){
+
+    // 0 -> relationalop
+    // 1 -> col
+    // 2 -> logicalop
+    // 3 -> string
+    // 4 -> Num
+
+    bool is_found = false;
+
+    vector<int> start_values;
+    vector<int> end_values;
+
+    for(int i = 0;i<values.size();i++){
+        if(((i==0) && (i == values.size()-1)) && ((values[i] == 0) || (values[i] == 2))){
+            return false;
+        }
+        else{
+            if(values[i] == 0){
+                if((values[i-1] != 1) || (values[i+1] == 0) || (values[i+1] == 2) || (values[i+1] == 1)){
+                    return false;
+                }
+                else{
+                    is_found = true;
+                    start_values.push_back(i-1);
+                    end_values.push_back(i+1);
+                    i++;
+                }
+            }
+        }
+    }
+
+    for(int i = 0;i<values.size();i++){
+        if(values[i] == 2){
+            int start_key = i+1;
+            int end_key = i-1;
+
+            bool found = false;
+            for(int j=0;j<start_values.size();j++){
+                if(start_key == start_values[j]){
+                    found = true;
+                    break;
+                }
+            }
+
+            if(found == false){
+                return false;
+            }
+            else{
+                found = false;
+                for(int j = 0;j<end_values.size();j++){
+                    if(end_key == end_values[j]){
+                        found = true;
+                        break;
+                    }
+                }
+                if(found == false){
+                    return false;
+                }
+                is_found = true;
+            }
+        }
+    }
+
+    if(is_found == false){
+        return false;
+    }
+    else{
+        return true;
+    }
+
+}
 
 
 
 int main(){
-
 
     vector<string> commands;
     commands.push_back("SELECT");
     commands.push_back("FROM");
     commands.push_back("WHERE");
 
-    // Will contain key_value pairs which can be used during parsing and evaluation
-    map<string,int> extended_environment;
-
     vector<string> initial_environment;
-    //intial environment will have all the column names specified in the table by default
-    // We are considering a simple table which has 4 columns and this can be extended to any number of columns
-    // 1. Item_Name - String (consisits of alphabets) (Data Type)
-    // 1. Item_Cost - int (Data Type)
-    // 1. Item_Quantity - int (Data Type)
-    // 1. Item_Color - String (consisits of alphabets) (Data Type)
     initial_environment.push_back("ItemName");
     initial_environment.push_back("ItemCost");
     initial_environment.push_back("ItemQuantity");
     initial_environment.push_back("ItemColor");
 
-
+    map<string,int> extended_environment;
 
     cout<<"Welcome to DSL for SQL Statement-Select on the table : "<<table_name<<endl<<endl;
+    cout<<"The columns present in the table are : ";
+
+    for(int i =0; i<initial_environment.size();i++){
+        cout<<initial_environment[i]<<" ";
+    }
+    cout<<endl<<endl;
+
     cout<<"Press q to quit"<<endl;
 
-    while(1){
+     while(1){
         cout<<endl;
-        cout<<"Enter the expression : "<<endl;
+        cout<<"Column:> ";
         string expression;
         getline(cin, expression);
-        //cin>>expression;
         cout<<endl;
-        cout<<"You have entered : "<<endl;
-        cout<<expression<<endl;
-
         if(expression == "q"){
             cout<<"Quitting . ."<<endl;
             break;
         }
-        string parsed_string = "";
-        bool is_parsed = parse_expression(expression,initial_environment,extended_environment,parsed_string);
-        string sql_statement= "";
-        if(is_parsed == true){
-            cout<<"Parsed String : "<<parsed_string<<endl<<endl;
-            cout<<"Evaluating . . "<<endl<<endl;
-            sql_statement = (interpret_expression(expression,initial_environment,extended_environment,commands) + ";");
-            cout<<"Evalauted Statement : "<<endl<<endl;
-            cout<<sql_statement<<endl;
+        //cout<<expression<<endl;
+        string parsed_string_expression = "";
+        bool flag = parse_column(expression,initial_environment,parsed_string_expression);
+        if(flag == true){
+            cout<<"Parsed Successfully : "<<parsed_string_expression<<endl;
+            vector<string> temp_conditions;
+            bool is_and = false;
+            bool is_or = false;
+            cout<<"Commands : "<<endl;
+            cout<<"D: Done"<<endl;
+            cout<<endl;
+            cout<<"Cond:> ";
+            string condition_expression;
+
+            getline(cin, condition_expression);
+
+            if(condition_expression == "D"){
+                string statement = interpret_expression(expression,commands);
+                statement += ";";
+                cout<<"Ouput:> "<<statement<<endl<<endl;
+            }
+            else{
+                vector<int> check_statements;
+                temp_conditions.push_back(condition_expression);
+                string parsed_string ="";
+                bool flag = parse_expression(condition_expression,initial_environment,extended_environment,parsed_string,check_statements);
+                if(flag == true){
+                    bool flag  = check_valid_condition (check_statements);
+                    if(flag == true){
+                        cout<<parsed_string<<endl<<endl;
+                        string statement_conditon = interpret_condition(condition_expression,commands);
+                        string statement = interpret_expression(expression,commands) + " " +statement_conditon + ";";
+                        cout<<"Ouput:> "<<statement<<endl<<endl;
+                    }
+                    else{
+                        cout<<"Error: Invalid Condition"<<endl;
+                    }
+
+                }
+            }
+            cout<<endl;
         }
+        else{
+            cout<<"Error: Invalid Column"<<endl;
+        }
+
+
+
 
     }
 
@@ -413,3 +479,4 @@ int main(){
 
 return 0;
 }
+
